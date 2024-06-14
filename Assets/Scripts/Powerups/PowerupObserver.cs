0b6c0765784f710fd595d425f8a6f2d885077727
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-06-14 21:52:26 +0800

    powerup sounds

--- Assets/Scripts/Powerups/PowerupObserver.cs
+++ Assets/Scripts/Powerups/PowerupObserver.cs
@@ -11,9 +11,15 @@ public class PowerupObserver : MonoBehaviour
     [SerializeField] private int jumpChangeAmount = 2;
     [SerializeField] MistHitPlayer mist;
 
+    [SerializeField] private AudioClip _collectPowerupSFX;
+    [SerializeField] private AudioClip _endPowerupSFX;
+
+    private AudioSource _audioPlayer;
 
     void Start()
     {
+        _audioPlayer = GetComponent<AudioSource>();
+
         EventBroadcaster.Instance.AddObserver(EventNames.Powerups.ON_COLLIDE_CANISTER, this.OnCollideCanister);
         EventBroadcaster.Instance.AddObserver(EventNames.Powerups.ON_COLLIDE_CAPSULE, this.OnCollideCapsule);
         EventBroadcaster.Instance.AddObserver(EventNames.Powerups.ON_COLLIDE_JETPACK, this.OnCollideJetpack);
@@ -30,6 +36,8 @@ public class PowerupObserver : MonoBehaviour
     {
         Debug.Log("canister");
         this.mist.damage = 0;
+        this._audioPlayer.clip = _collectPowerupSFX;
+        this._audioPlayer?.Play();
         Invoke("OnCanisterEnd", timeEndPowerup);
     }
 
@@ -38,6 +46,8 @@ public class PowerupObserver : MonoBehaviour
         Rigidbody Mass = this.Player.GetComponent<Rigidbody>();
         if(Mass != null) Mass.mass -= massChangeAmount;
         Debug.Log("capsule");
+        this._audioPlayer.clip = _collectPowerupSFX;
+        this._audioPlayer?.Play();
         Invoke("OnCapsuleEnd", timeEndPowerup);
     }
 
@@ -46,12 +56,16 @@ public class PowerupObserver : MonoBehaviour
         PlayerMovement Jump = this.Player.GetComponent<PlayerMovement>();
         if (Jump != null) Jump.AddMaxAirJumps(jumpChangeAmount);
         Debug.Log("jetpack");
+        this._audioPlayer.clip = _collectPowerupSFX;
+        this._audioPlayer?.Play();
         Invoke("OnJetpackEnd", timeEndPowerup);
     }
 
     private void OnCanisterEnd()
     {
         this.mist.damage = 10;
+        this._audioPlayer.clip = _endPowerupSFX;
+        this._audioPlayer?.Play();
         Debug.Log("end canister");
     }
 
@@ -59,6 +73,8 @@ public class PowerupObserver : MonoBehaviour
     {
         Rigidbody Body = this.Player.GetComponent<Rigidbody>();
         if (Body != null) Body.mass += massChangeAmount;
+        this._audioPlayer.clip = _endPowerupSFX;
+        this._audioPlayer?.Play();
         Debug.Log("end capsule");
     }
 
@@ -66,6 +82,8 @@ public class PowerupObserver : MonoBehaviour
     {
         PlayerMovement Jump = this.Player.GetComponent<PlayerMovement>();
         if (Jump != null) Jump.AddMaxAirJumps(-jumpChangeAmount);
+        this._audioPlayer.clip = _endPowerupSFX;
+        this._audioPlayer?.Play();
         Debug.Log("end jetpack");
     }
 }
